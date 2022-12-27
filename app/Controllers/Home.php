dd48cc8c98448bd2e95fd8d84349e422757c885f
Roland Vincent <roland.vincentnet@gmail.com>
2022-12-27 11:11:16 +0700

    Add basic template

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,10 @@ class Home extends BaseController
 {
     public function index()
     {
-        return view('welcome_message');
+        $data = [
+            "title" => "Beranda",
+            "menu_select" => 1
+        ];
+        return view('welcome_message', $data);
     }
 }
